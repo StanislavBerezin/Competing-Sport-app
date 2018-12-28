ed4bece3c75fb764331ee7bfee89add731979a22
StanislavBerezin <stanislav_berezin@hotmail.com>
2018-12-28 19:46:01 +1000

    Created classess

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Represents a team in a matchup
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+        /// <summary>
+        /// The score of this team
+        /// </summary>
+        public double Score { get; set; }
+        /// <summary>
+        /// Where it came from
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
